18a26fc891a86271cdd54b708e91dd766396d86f
idrisseaa <idrisseaa93@gmail.com>
2025-01-29 00:45:11 +0100

    Added the ForgotPasswordScreen

--- navigation/StackNavigation.js
+++ navigation/StackNavigation.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import OnBoardingScreen from "../src/screens/OnBoardingScreen";
 import LoginScreen from "../src/screens/LoginScreen";
 import SignupScreen from "../src/screens/SignupScreen";
+import ForgotPasswordScreen from "../src/screens/ForgotPasswordScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +15,7 @@ export default function StackNavigation() {
                 <Stack.Screen name="OnBoarding" component={OnBoardingScreen} />
                 <Stack.Screen name="LoginScreen" component={LoginScreen} />
                 <Stack.Screen name="SignupScreen" component={SignupScreen} />
+                <Stack.Screen name="ForgotPasswordScreen" component={ForgotPasswordScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
