f0a6a6f5d572ae00992239f0787266aea2472174
idrisseaa <idrisseaa93@gmail.com>
2025-01-28 23:04:53 +0100

    Structed a little bit. and added constants

---
+++ src/constants/Images.js
@@ -0,0 +1,7 @@
+export const Images = {
+    // Images for the onboarding part
+    amigo: require('../../assets/images/onboarding-1.png'),
+    bro: require('../../assets/images/onboarding-2.png'),
+    rafiki:require('../../assets/images/onboarding-3.png'),
+
+};
